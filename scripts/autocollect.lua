695ecfcca0963d2edd3388582466254fdc0d8ac4
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2026-06-23 19:59:22 -0400

    Enhance fruit collection process with notifications
    Added notification functionality and improved collection process logic.

--- scripts/autocollect.lua
+++ scripts/autocollect.lua
@@ -9,6 +9,10 @@ local Modules = {
 	Networking = require(ReplicatedStorageService:WaitForChild("SharedModules"):FindFirstChild("Networking"))
 }
 
+local Remotes = {
+	Notify = ReplicatedStorageService:WaitForChild("Notify")
+}
+
 local Folders = {
 	Gardens = workspace:WaitForChild("Gardens")
 }
@@ -16,6 +20,8 @@ local Folders = {
 local LocalPlayer = PlayersService.LocalPlayer
 
 local plotId = LocalPlayer:GetAttribute("PlotId")
+local fruitCount = LocalPlayer:GetAttribute("FruitCount")
+local maxFruits = LocalPlayer:GetAttribute("MaxFruitCapacity")
 
 local garden = Folders.Gardens:FindFirstChild(string.format("Plot%i", plotId))
 
@@ -23,20 +29,39 @@ local plants = garden:FindFirstChild("Plants")
 
 Modules.Networking.TeleportButton.Request:Fire("Garden")
 
+Remotes.Notify:Fire("Starting collection process . . .")
+
+task.wait(1)
+
 for _, plant in plants:GetChildren() do
+	if fruitCount >= maxFruits then continue end
+
 	local fruits = plant:FindFirstChild("Fruits")
 
 	if not fruits then continue end
 
 	for _, fruit in fruits:GetChildren() do
+		if fruitCount >= maxFruits then continue end
+
 		local plantId = fruit:GetAttribute("PlantId")
 		local fruitId = fruit:GetAttribute("FruitId")
 
 		-- print(string.format("Plant: %s\n\t\t\tFruit: %s", plantId, fruitId))
 		Modules.Networking.Garden.CollectFruit:Fire(plantId, fruitId)
+		fruitCount = LocalPlayer:GetAttribute("FruitCount")
 
 		task.wait()
 	end
+end
+
+fruitCount = LocalPlayer:GetAttribute("FruitCount")
+
+task.wait(1)
+
+if fruitCount >= maxFruits then
+	Remotes.Notify:Fire("Your inventory is full! Teleporting to sell stand . . .")
+else
+	Remotes.Notify:Fire("Finished collection process. Teleporting to sell stand . . .")
 end
 
 Modules.Networking.TeleportButton.Request:Fire("Sell")
